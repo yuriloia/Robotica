de16821794bd6cc77f366262f738cf999abd1f39
Yuri Loia <37004692+yuriloia@users.noreply.github.com>
2018-03-22 09:02:35 -0300

    Adicionado alguns elementos extras

--- Teste_imagem.m
+++ Teste_imagem.m
@@ -3,11 +3,11 @@
 %Ip da Câmera
 clear all
 
-mylego = legoev3('usb');
-mymotor = motor(mylego,'A');
+% mylego = legoev3('usb');
+% mymotor = motor(mylego,'C');
+% mymotor1 = motor(mylego,'B');
 
-
-url = 'http://192.168.0.102:8080/shot.jpg';
+url = 'http://192.168.0.107:8080/shot.jpg';
 
 %Leitura da imagem fornecida pelo Ip da câmera 
 ss  = imread(url);
@@ -32,16 +32,24 @@ while(1)
     i = i + 1;
   
     
-    ss(221:260, 299:300, :) = 0;
-    ss(221:260, 299:300, 3) = 255;
-    ss(221:260, 341:342, :) = 0;
-    ss(221:260, 341:342, 3) = 255;
-    ss(219:220, 301:340, :) = 0;
-    ss(219:220, 301:340, 3) = 255;
-    ss(261:262, 301:340, :) = 0;
-    ss(261:262, 301:340, 3) = 255;
+    ss(221:221+quadro-1, 299:300, :) = 0;
+    ss(221:221+quadro-1, 299:300, 3) = 255;
+    ss(221:221+quadro-1, 341:342, :) = 0;
+    ss(221:221+quadro-1, 341:342, 3) = 255;
+    ss(219:220, 301:301+quadro-1, :) = 0;
+    ss(219:220, 301:301+quadro-1, 3) = 255;
+    ss(261:262, 301:301+quadro-1, :) = 0;
+    ss(261:262, 301:301+quadro-1, 3) = 255;
     k = 0;
     
+    ss(80:81+quadro-1, 179:180, :) = 0;
+    ss(80:81+quadro-1, 179:180, 3) = 255;
+    ss(80:81+quadro-1, 220:221, :) = 0;
+    ss(80:81+quadro-1, 220:221, 3) = 255;
+    ss(120:121, 181:181+quadro-1, :) = 0;
+    ss(120:121, 181:181+quadro-1, 3) = 255;
+    ss(79:80, 181:181+quadro-1, :) = 0;
+    ss(79:80, 181:181+quadro-1, 3) = 255;
     tic
     %Contagem de blobs pretos, ou em escalas de cinza, para detecção 
     %"n" indica as linhas e "j" as colunas
@@ -53,25 +61,30 @@ while(1)
             
             %ss(n-1,j-1,:) = 0;
             %ss(n-1,j-1,3) = 255;
-            if A(n,j) < 50
+            if A(n,j) < 120
                 k = k + 1;    
             end
         end
     end
-    k
+    %k
     %Se tiver mais de 1400 pixels pretos acontece a detecção e
     %consequentemente há o "beep" no lego.
     
     set(fh,'CData',ss);
     drawnow;
-    
+    a = 100;
+    b = 200;
     if (k >= 1400)
+        a
         %beep(mylego);
-        mymotor.Speed = 20; % velocidade do motor
-        start(mymotor); %iniciar
+%         mymotor.Speed = 20; % velocidade do motor
+%         mymotor1.Speed = 20;
+%         start(mymotor); %iniciar
+%         start(mymotor1);
     else
-        mymotor.Speed = -20; % velocidade do motor reversa
-        start(mymotor); %iniciar
+        b
+%         stop(mymotor);
+%         stop(mymotor1);
     end
     toc
 end
